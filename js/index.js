934a8415473b01095ecf8ecae92cc8f62a7cab49
leeeseokjun <109326314+leeeseokjun@users.noreply.github.com>
2022-08-26 16:57:19 +0900

    universallist
    universallist renewal

---
+++ js/index.js
@@ -0,0 +1,27 @@
+$("document").ready(function(){
+    $("label").click(function(){
+        $(".m-box").slideToggle(500);
+    }) //햄버거
+
+    
+    $(".main-content .content-item .item").click(function(){
+        let i = $(this).index()
+        $(".main-content .content-item .item").removeClass("on").eq(i).addClass("on")
+        $(".content .item-wrap .menu .i").hide().eq(i).show()
+    })
+    $(".main-content .content-item .item").removeClass("on").eq(0).addClass("on")
+    $(".content .item-wrap .menu .i").hide().eq(0).show()
+
+    //메인 컨텐츠
+
+    $(".content .menu .item-new .text ul li").click(function(){
+        let i = $(this).index()
+        $(".content .menu .item-new .text ul li").removeClass("on").eq(i).addClass("on")
+        $(".content .menu .item-new .product").hide().eq(i).css('display','flex')
+        $(".content .menu .item-new .selected").hide().eq(i).show()    
+    })
+    $(".content .menu .item-new .text ul li").removeClass("on").eq(0).addClass("on")
+    $(".content .menu .item-new .product .product-item img").show()
+    $(".content .menu .item-new drake").show()
+    //new arrival
+})
